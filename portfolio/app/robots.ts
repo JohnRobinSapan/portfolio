eed3ed7674e16ea02ec77a3a10ff34bb01068e4b
JohnRobinSapan <sapanjo@sheridancollege.ca>
2024-02-22 19:44:22 -0500

    create portfolio skeleton

---
+++ portfolio/app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://johnsapan.io/sitemap.xml',
+    host: 'https://johnsapan.io',
+  };
+}
